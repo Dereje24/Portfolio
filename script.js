b16dc87c18652c50740f1e4358c13098ec7ade9b
Dereje24 <ddilnesaw@yahoo.com>
2018-02-09 20:57:04 -0800

    have two carousels css layout changed

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
   var slideIndex = 0;
 // gallery is parent container for all images & .each goes throught any element with the class gallery. 
   $('.gallery').each(function(i, el){
-    startSlideShow(el);
+    slideShow(el);
 // this function will run on the element element that have class gallery 
   });
 
-  function startSlideShow(el) {
+  function slideShow(el) {
   	var slides = $(el).children('.slides');
 // this ^ is selecting the children of gallery that have class slides.
   	  for ( var i = 0; i < slides.length; i++) {
@@ -19,9 +19,9 @@ $(document).ready(function(){
 // incrementing our stating position form 0
       slides[slideIndex % slides.length].style.display = "block"; 
 // get the slide at index 0,1 or 2 and change display from none to block.  
-      setTimeout(function() {startSlideShow(el)}, 5000);
+      setTimeout(function() {slideShow(el)}, 5000);
   };
-// this is calling startSlideShow function every 5 seconds!
+// this is calling slideShow function every 5 seconds!
 
   $('.menu-btn').on('click', function(e){
   	e.preventDefault();
