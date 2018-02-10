ddbf60a04bb40be47bad20cf5f6db70dc5b0b18c
Dereje24 <ddilnesaw@yahoo.com>
2018-02-09 18:33:36 -0800

    got rid of the border color for main. added a second carousel. nav bar changed to no padding.

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 $(document).ready(function(){
 	console.log('ready')
 
-	var slideIndex = 0;
-slideShow();
-function slideShow() {
-	var slide = $('.slides');
-	 for ( var i = 0; i < slide.length; i++) {
-       slide[i].style.display = "none";  
-    }	
-    slideIndex ++;
+  var slideIndex = 0;
+// gallery is parent container for all images & .each goes throught any element with the class gallery. 
+  $('.gallery').each(function(i, el){
+    startSlideShow(el);
+// this function will run on the element element that have class gallery 
+  });
 
-    if (slideIndex > slide.length) {
-    		slideIndex = 1
-    	};
+  function startSlideShow(el) {
+  	var slides = $(el).children('.slides');
+// this ^ is selecting the children of gallery that have class slides.
+  	  for ( var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+// for loop is itterating through each slide and hidding all of them. 
+      slideIndex ++;
+// incrementing our stating position form 0
+      slides[slideIndex % slides.length].style.display = "block"; 
+// get the slide at index 0,1 or 2 and change display from none to block.  
+      setTimeout(function() {startSlideShow(el)}, 5000);
+  };
+// this is calling startSlideShow function every 5 seconds!
 
-    slide[slideIndex-1].style.display = "block"; 
-    setTimeout(slideShow, 5000);
-
-};
-
-$('.menu-btn').on('click', function(e){
-	e.preventDefault();
-	console.log(this);
-   	$('nav').toggle('expand');
-   	})
+  $('.menu-btn').on('click', function(e){
+  	e.preventDefault();
+  	console.log(this);
+     	$('nav').toggle('expand');
+  })
 });
 
